3f47102ac094279f8f32d9453b581e4b36932ebd
BenjaCimatti <BenjaCimatti>
2020-10-25 23:51:48 -0300

    Fix de código redundante

--- Torneos/AdministradorTorneos/urls.py
+++ Torneos/AdministradorTorneos/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
-    path('home/', views.homePage, name='login'),
+    path('home/', views.homePage, name='home'),
+    path('', RedirectView.as_view(url='login/')),
+
 ]
