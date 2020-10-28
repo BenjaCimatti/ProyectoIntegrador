3d401d9201644415a0c4a28ecdd7f6484430dc6b
LucasPasolli <lippasolli@hotmail.com>
2020-10-28 12:14:30 -0300

    Logout Funcional y Restricciones de Vistas

--- Torneos/AdministradorTorneos/urls.py
+++ Torneos/AdministradorTorneos/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('home/', views.homePage, name='home'),
     path('', RedirectView.as_view(url='login/')),
 
--- Torneos/AdministradorTorneos/views.py
+++ Torneos/AdministradorTorneos/views.py
@@ -5,39 +5,51 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required(login_url='login')
 def homePage(request):
     return render(request, 'AdministradorTorneos/home.html')
 
 def registerPage(request):
-    form = CreateUserForm()
+    
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Cuenta creada correctamente')
+            return redirect('login')
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-    if form.is_valid():
-        form.save()
-        messages.success(request, 'Cuenta creada correctamente')
-        return redirect('login')
-
-    context = {'form':form}
-    return render(request, 'AdministradorTorneos/register.html', context)
+        context = {'form':form}
+        return render(request, 'AdministradorTorneos/register.html', context)
 
     
 def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
 
-        user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
+            else:
+                messages.info(request, 'El usuario o la contraseña es incorrecto')
 
-        else:
-            messages.info(request, 'El usuario o la contraseña es incorrecto')
+        context = {}
+        return render(request, 'AdministradorTorneos/login.html', context)
 
-    context = {}
-    return render(request, 'AdministradorTorneos/login.html', context)
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
