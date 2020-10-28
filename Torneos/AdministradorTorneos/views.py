da26dce08b2915498ad1c241adb5a9e4984597ef
LucasPasolli <lippasolli@hotmail.com>
2020-10-28 09:58:44 -0300

    Adicion de mensaje de confirmacion de creacion de cuenta

--- Torneos/AdministradorTorneos/views.py
+++ Torneos/AdministradorTorneos/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def homePage(request):
@@ -13,8 +15,10 @@ def registerPage(request):
 
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Cuenta creada correctamente')
+        return redirect('login')
 
     context = {'form':form}
     return render(request, 'AdministradorTorneos/register.html', context)
