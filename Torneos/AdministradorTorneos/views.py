b27e593529766424b297dea603e324ec11bed89d
LucasPasolli <lippasolli@hotmail.com>
2020-10-28 09:15:54 -0300

    Register Funcional

--- Torneos/AdministradorTorneos/views.py
+++ Torneos/AdministradorTorneos/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.http import HttpResponse
-
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 # Create your views here.
 
 def homePage(request):
     return render(request, 'AdministradorTorneos/home.html')
 
 def registerPage(request):
-    context = {}
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
     return render(request, 'AdministradorTorneos/register.html', context)
 
 def loginPage(request):
