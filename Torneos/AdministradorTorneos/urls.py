477a6c5af5343b9e2595b04cf7d1b6dd9a96c2ea
LucasPasolli <lippasolli@hotmail.com>
2020-10-28 16:59:46 -0300

    Adicion de templates de cambiar contraseña

--- Torneos/AdministradorTorneos/urls.py
+++ Torneos/AdministradorTorneos/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
     path('home/', views.homePage, name='home'),
     path('', RedirectView.as_view(url='login/')),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    #cambiar contraseña
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="AdministradorTorneos/password_reset.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="AdministradorTorneos/password_reset_sent.html"), name="password_reset_done"),
     path('reset_password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
