a334d838a31bc26878e650a91e3d5b5919dc2b89
BenjaCimatti <benja.cimatti@gmail.com>
2020-10-30 22:39:17 -0300

    Idea de models implementada

--- Torneos/AdministradorTorneos/admin.py
+++ Torneos/AdministradorTorneos/admin.py
@@ -4,12 +4,34 @@ from django.contrib import admin
 
 from .models import *
 
+class QuarterMatchlInline(admin.TabularInline):
+    model = QuarterMatch
+    fields = ['match_map',]
+    extra = 4
+
+class SemiMatchlInline(admin.TabularInline):
+    model = SemiMatch
+    fields = ['match_map',]
+    extra = 2
+
+class FinalMatchlInline(admin.TabularInline):
+    model = FinalMatch
+    fields = ['match_map',]
+    extra = 1
+
 class PlayerAdmin(admin.ModelAdmin):
     readonly_fields = ["id","user"]
+
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'game')
+    list_display_links = ('name', 'game')
+    inlines = [QuarterMatchlInline, SemiMatchlInline, FinalMatchlInline]
 
     
 
 admin.site.register(Game)
-#admin.site.register(Tournament)
+admin.site.register(Tournament, TournamentAdmin)
 admin.site.register(Player)
-#admin.site.register(Match)
+admin.site.register(QuarterMatch)
+admin.site.register(SemiMatch)
+admin.site.register(FinalMatch)
