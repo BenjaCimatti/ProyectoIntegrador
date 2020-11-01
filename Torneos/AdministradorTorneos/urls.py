3407124f8d5b9004fdc4458e29bfdfa30a98564f
LucasPasolli <lippasolli@hotmail.com>
2020-11-01 12:14:29 -0300

    Pagina Password reset form

--- Torneos/AdministradorTorneos/urls.py
+++ Torneos/AdministradorTorneos/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     #cambiar contraseña
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="AdministradorTorneos/password_reset.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="AdministradorTorneos/password_reset_sent.html"), name="password_reset_done"),
-    path('reset_password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="AdministradorTorneos/password_reset_form.html"), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
